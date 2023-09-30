a539d7e068aa090f582d408531b1d84d7d62f117
Jorge Pardo <jorgeparpar@gmail.com>
2023-09-30 20:20:19 +0200

    feat: more stuff

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -7,18 +7,35 @@ interface Project {
     description: string;
     technologies: string[];
     icon: string;
+    status: string;
 }
 
 interface ProjectCardProps {
-    project: Project
+    project: Project;
 }
 
 export default function ProjectCard(props: ProjectCardProps): JSXElement {
     return (
-        <div class='project'>
+        <div
+            class='project'
+            onClick={() => {
+                window.open(props.project.url);
+            }}
+        >
             <img src={props.project.icon} alt='' />
             <h3>{props.project.name}</h3>
             <p>{props.project.description}</p>
+            <div class='project-label'>Take me there!</div>
+            <div
+                class='project-status'
+                classList={{
+                    active: props.project.status === 'active',
+                    onhold: props.project.status === 'on_hold',
+                }}
+            >
+                <div class='status-icon' />
+                <span>{props.project.status === 'active' ? 'Active' : 'On Hold'}</span>
+            </div>
         </div>
     );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,13 +4,22 @@ import { For, type JSXElement } from 'solid-js';
 import ProjectCard from '~/components/ProjectCard';
 import projects from '../data/projects.json';
 
+const statusOrder = { active: 1, on_hold: 2, abandoned: 3 };
+
 export default function Home(): JSXElement {
     return (
         <main>
             <img id='pfp' src={ProfilePicture} />
-            <h1>Hi, I'm Jorge Pardo</h1>
+            <h1>Hi, I'm Jorge Pardo.</h1>
             <div class='project-container'>
-                <For each={projects}>
+                <For
+                    each={projects.sort((a, b) => {
+                        const statusA = statusOrder[a.status];
+                        const statusB = statusOrder[b.status];
+
+                        return statusA - statusB;
+                    })}
+                >
                     {(project) => <ProjectCard project={project} />}
                 </For>
             </div>
