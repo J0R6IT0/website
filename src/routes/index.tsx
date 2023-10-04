bf633ee5e0753856d4606182cc167bba8656a485
Jorge Pardo <jorgeparpar@gmail.com>
2023-10-04 12:13:06 +0200

    feat: make the pfp shrink/grow on scroll

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,8 +7,22 @@ import projects from '../data/projects.json';
 const statusOrder = { active: 1, on_hold: 2, abandoned: 3 };
 
 export default function Home(): JSXElement {
+    const handleScroll = (): void => {
+        const image = document.getElementById('pfp');
+        if (image !== null) {
+            if (document.body.firstElementChild?.scrollTop !== undefined) {
+                image.style.width = `${Math.max(
+                    10 * 2 - document.body.firstElementChild?.scrollTop / 10,
+                    10
+                )}rem`;
+            } else {
+                image.style.width = '18rem';
+            }
+        }
+    };
+
     return (
-        <main>
+        <main onScroll={handleScroll}>
             <img id='pfp' src={ProfilePicture} />
             <h1>Hi, I'm Jorge Pardo.</h1>
             <div class='project-container'>
