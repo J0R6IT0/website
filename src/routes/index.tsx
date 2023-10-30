943d86b88f8a59ec313e96ef77fac75de8b74691
J0R6IT0 <jorgeparpar@gmail.com>
2023-10-30 20:58:20 +0100

    feat: tech carousel

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,45 @@
 import './index.scss';
-import { type JSXElement } from 'solid-js';
+import { For, type JSXElement } from 'solid-js';
 import Header from '~/components/Header';
+import profilePicture from '../assets/images/pfp.webp';
+import javascript from '../assets/icons/tech/javascript.svg';
+import typescript from '../assets/icons/tech/typescript.svg';
+import reactjs from '../assets/icons/tech/reactjs.svg';
+import rust from '../assets/icons/tech/rust.svg';
+import solidjs from '../assets/icons/tech/solidjs.svg';
+import java from '../assets/icons/tech/java.svg';
+import mongodb from '../assets/icons/tech/mongodb.svg';
+
+const techs = [
+    { name: 'Rust', icon: rust },
+    { name: 'Java', icon: java },
+    { name: 'JavaScript', icon: javascript },
+    { name: 'TypeScript', icon: typescript },
+    { name: 'ReactJS', icon: reactjs },
+    { name: 'SolidJS', icon: solidjs },
+    { name: 'MongoDB', icon: mongodb },
+];
 
 export default function Home(): JSXElement {
     return (
         <main>
-            <Header activePage='Home'/>
-            home
+            <Header activePage='Home' />
+            <div id='details'>
+                <img src={profilePicture} alt='' />
+                <span id='name'>Hi, I'm Jorge Pardo.👋</span>
+                <span id='quote'>
+                    <p>Building software with passion.</p>
+                    <p>Prioritizing user experience and attention to detail.</p>
+                </span>
+            </div>
+            <div id='carousel'>
+                <div id='carousel-slide'>
+                    <For each={techs}>{(tech) => <img src={tech.icon} />}</For>
+                </div>
+                <div id='carousel-slide'>
+                    <For each={techs}>{(tech) => <img src={tech.icon} />}</For>
+                </div>
+            </div>
         </main>
     );
 }
